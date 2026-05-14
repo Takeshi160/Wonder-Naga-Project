812db5ddfbf8e513d3df19402e6aea9a7358fcba
Takeshi160 <seanjozefvibar@gmail.com>
2026-05-15 07:44:42 +0800

    Change SECRET_KEY and clean up init.py
    Updated SECRET_KEY and removed unnecessary comments.

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,23 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key')
+app.config['SECRET_KEY'] = 'thatsmynaga'
 
 database_url = os.environ.get('DATABASE_URL')
 
-# Fix old postgres:// format
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
 
-# Force psycopg driver
-if database_url and database_url.startswith("postgresql://"):
-    database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-login = LoginManager(app)
-login.login_view = 'login'
+migrate = Migrate(app, db)
 
 from app import routes, models
