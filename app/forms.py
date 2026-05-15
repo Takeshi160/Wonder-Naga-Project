b3c7d86c2cc2b2d242ccf8b56f20bc6daa99cdd5
Rhyle Matthew <rhylematthew0509@gmail.com>
2026-05-15 14:06:57 +0800

    FIXED MANY STUFF

--- app/forms.py
+++ app/forms.py
@@ -6,45 +6,28 @@ from app import db
 from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField(
-        "Username",
-        validators=[DataRequired()],
-        render_kw={"class": "form-control", "autocomplete": "username"}
-    )
-    password = PasswordField(
-        "Password",
-        validators=[DataRequired()],
-        render_kw={"class": "form-control", "autocomplete": "current-password"}
-    )
-    remember_me = BooleanField(
-        "Remember Me",
-        render_kw={"class": "form-check-input"}
-    )
-    submit = SubmitField(
-        "Sign In",
-        render_kw={"class": "btn btn-primary"}
-    )
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        user = db.session.scalar(sa.select(User).where(
-            User.username == username.data))
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
         if user is not None:
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
-        user = db.session.scalar(sa.select(User).where(
-            User.email == email.data))
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError('Please use a different email address.')
-        
+
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
@@ -56,27 +39,29 @@ class EditProfileForm(FlaskForm):
 
     def validate_username(self, username):
         if username.data != self.original_username:
-            user = db.session.scalar(sa.select(User).where(
-                User.username == username.data))
+            user = db.session.scalar(sa.select(User).where(User.username == username.data))
             if user is not None:
                 raise ValidationError('Please use a different username.')
-            
+
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)])
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
 
 class RecommendationForm(FlaskForm):
-    title = StringField('Name', validators=[DataRequired()])
-    reason = StringField('Reason for Recommending', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired()])
-    category = SelectField('Category', validators=[DataRequired()], choices=[])
+    name = StringField('Store Name', validators=[DataRequired()])
+    reason = StringField('Reason for Recommending')
+    location = StringField('Store Location', validators=[DataRequired()])
+    category = SelectField('Category', validators=[DataRequired()], choices=[
+        ('restaurant', 'Restaurant'),
+        ('cafe', 'Cafe'),
+        ('shop', 'Shop'),
+        ('attraction', 'Attraction'),
+        ('other', 'Other')
+    ])
     hours = StringField('Operating Hours')
     contact = StringField('Contact Number')
     description = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Add Recommendation')
-
-    
